db826d5989789aff66303aec3496ee5a7cf7b611
YarValeriy <147444561+YarValeriy@users.noreply.github.com>
2023-12-01 22:56:28 +0200

    Add files via upload

--- Sort_files.py
+++ Sort_files.py
@@ -71,13 +71,14 @@ def normalize(string):
 
 
 def sort_files(folder_path):
+    n_files = 0
     # Define folder names for each category
     categories = {
-        "Image": ["JPEG", "PNG", "JPG", "SVG"],
+        "Images": ["JPEG", "PNG", "JPG", "SVG"],
         "Video": ["AVI", "MP4", "MOV", "MKV"],
-        "Document": ["DOC", "DOCX", "TXT", "PDF", "XLS", "XLSX", "PPT", "PPTX"],
+        "Documents": ["DOC", "DOCX", "TXT", "PDF", "XLS", "XLSX", "PPT", "PPTX"],
         "Audio": ["MP3", "OGG", "WAV", "AMR", "M4A"],
-        "Archive": ["ZIP", "GZ", "TAR"],
+        "Archives": ["ZIP", "GZ", "TAR"],
         "Code": ["PY", "CPP", "CXX", "CC"],
         "Markup": ["XML", "HTML", "CSS"],
         "Unknown": [],
@@ -96,30 +97,39 @@ def sort_files(folder_path):
         dirs[:] = [
             d for d in dirs if not d in list(categories.keys())
         ]  # skip service folders
+
         for file in files:
-            extension = file.split(".")[-1].upper()
-            n_file = normalize(file.split(".")[0]) + "." + file.split(".")[-1]
+            split_name = file.split(".")
+            if len(split_name) == 1:
+                file_ext = ""
+                n_file = normalize(split_name[0])
+            else:
+                file_ext = split_name[-1].upper()
+                n_file = normalize(split_name[0]) + "." + file_ext.lower()
             file_path = os.path.join(root, file)
 
             # Find the category for the file
             file_category = "Unknown"
             for category, extensions in categories.items():
-                if extension in extensions:
+                if file_ext in extensions:
                     file_category = category
                     break
+            if file_category == "Unknown":
+                categories["Unknown"].append(file_ext)
 
             # Move the file to the appropriate folder
             destination_folder = os.path.join(folder_path, file_category)
             shutil.move(file_path, os.path.join(destination_folder, n_file))
+            n_files += 1
 
     # Unpack archive files
-    archives_folder = os.path.join(folder_path, "Archive")
-    for root, dirs, files in os.walk(os.path.join(folder_path, "Archive")):
+    archives_folder = os.path.join(folder_path, "Archives")
+    for root, dirs, files in os.walk(os.path.join(folder_path, "Archives")):
         for file in files:
             file_path = os.path.join(root, file)
             archive_name = file.split(".")[0]
             archive_folder = os.path.join(archives_folder, archive_name)
-            print(archive_folder)
+            # print(archive_folder)
 
             # Create a subfolder for the archive if it doesn't exist
             if not os.path.exists(archive_folder):
@@ -127,13 +137,19 @@ def sort_files(folder_path):
 
             # Unpack the archive and move its contents to the subfolder
             if file.endswith(".zip"):
-                with zipfile.ZipFile(file_path, "r") as zip_ref: 
-                    zip_ref.extractall(archive_folder)  # names not nornmalized
-                os.remove(file_path)
+                with zipfile.ZipFile(file_path, "r") as zip_ref:
+                    try:
+                        zip_ref.extractall(archive_folder)  # names not nornmalized
+                        os.remove(file_path)
+                    except IOError as e:
+                        print(f"Archive {file_path} corrupted!", e)
             elif file.endswith(".tar") or file.endswith(".gz"):  # never tested
                 with tarfile.open(file_path, "r") as tar_ref:  # never tested
-                    tar_ref.extractall(archive_folder)  # never tested
-                os.remove(file_path)
+                    try:
+                        tar_ref.extractall(archive_folder)  # never tested
+                        os.remove(file_path)
+                    except IOError as e:
+                        print(f"Archive {file_path} corrupted!", e)
 
     # Remove empty folders
     for root, dirs, _ in os.walk(folder_path, topdown=False):
@@ -149,6 +165,7 @@ def sort_files(folder_path):
                 # os.rmdir(folder_path)
                 shutil.rmtree(folder_path, ignore_errors=True)
                 # print(f"{folder_path} deleted")
+    return (n_files, categories["Unknown"])
 
 
 if __name__ == "__main__":
@@ -156,5 +173,7 @@ if __name__ == "__main__":
         folder_to_sort = input("Enter the folder path: ")
     else:
         folder_to_sort = sys.argv[1]
-    sort_files(folder_to_sort)
-    print(f"Files at {folder_to_sort} sorted successfully")
+    response = sort_files(folder_to_sort)
+    print(
+        f"Files at {folder_to_sort} sorted successfully, {response[0]} files relocated, unknown extensions: {response[1]}"
+    )
